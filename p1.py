b236369a32565795a59d64ab5935b1500f0a3c95
Mohamed Hany <42424592+SoulRe@users.noreply.github.com>
2023-02-05 21:31:58 +0200

    Delete p1.py

--- p1.py
+++
@@ -1,9 +0,0 @@
-def main():
-    items = list(map(int, input("input: \n").split()))
-    items.sort()
-    print("output:")
-    print(items[len(items) - 2])
-    print(items[1])
-    
-    
-main()
